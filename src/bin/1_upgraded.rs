88c88d33255001bf24cc830cab459d1b9de8bcc2
Antoine Jacques-Jourion <52408308+axnj2@users.noreply.github.com>
2025-01-20 23:30:34 +0100

    Day 2 step 1 done, step 2 wrong

--- src/bin/1_upgraded.rs
+++ src/bin/1_upgraded.rs
@@ -2,23 +2,19 @@ use itertools::Itertools;
 use std::fs;
 
 // learned from https://github.com/voberle/adventofcode/blob/main/2024/day01/src/main.rs
+// when trying to bench mark the difference,
+// I found virtually no execution time difference at both 15 ms
 
 fn main() {
     let exemple_input = fs::read_to_string("inputs/1_e.txt").expect("The file should have opened");
     let exemple_lists = extract_number_lists(exemple_input);
 
-    println!(
-        "exemple : {}",
-        part1(&exemple_lists.0, &exemple_lists.1)
-    );
+    println!("exemple : {}", part1(&exemple_lists.0, &exemple_lists.1));
 
     let full_input = fs::read_to_string("inputs/1.txt").unwrap();
     let full_lists = extract_number_lists(full_input);
 
-    println!(
-        "step 1 : {}",
-        part1(&full_lists.0, &full_lists.1)
-    );
+    println!("step 1 : {}", part1(&full_lists.0, &full_lists.1));
 
     println!(
         "step 2 exemple : {}",
@@ -29,7 +25,7 @@ fn main() {
 }
 
 fn extract_number_lists(input: String) -> (Vec<i32>, Vec<i32>) {
-    let split_string: (Vec<i32>, Vec<i32>) = input
+    input
         .lines()
         .map(|line| {
             line.trim()
@@ -39,9 +35,7 @@ fn extract_number_lists(input: String) -> (Vec<i32>, Vec<i32>) {
                 .collect_tuple()
                 .unwrap()
         })
-        .unzip();
-
-    return split_string;
+        .unzip()
 }
 
 fn part1(first_list: &Vec<i32>, second_list: &Vec<i32>) -> u32 {
@@ -54,17 +48,6 @@ fn part1(first_list: &Vec<i32>, second_list: &Vec<i32>) -> u32 {
 }
 
 fn part2(first_list: &Vec<i32>, second_list: &Vec<i32>) -> i32 {
-    // let mut final_value = 0;
-    // for value1 in first_list {
-    //     final_value += second_list
-    //         .clone()
-    //         .into_iter()
-    //         .filter(|value2| *value2 == value1)
-    //         .sum::<i32>()
-    // }
-
-    // return final_value;
-
     first_list
         .iter()
         .map(|e1| second_list.iter().filter(|e2| *e2 == e1).sum::<i32>())
---
+++ src/bin/2.rs
@@ -0,0 +1,100 @@
+use std::collections::HashSet;
+use std::fs;
+use std::iter::Iterator;
+
+
+fn preprocess(input: &String) -> Vec<Vec<u32>> {
+    input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(|e| e.parse::<u32>().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+fn count_safe_reports(reports: &Vec<Vec<u32>>) -> u32 {
+    // let intermediary_result: Vec<_> = reports
+    //     .iter()
+    //     .filter(|report| {
+    //         (report.is_sorted() || report.iter().rev().is_sorted()) && {
+    //             let mut uniq = std::collections::HashSet::new();
+    //             // HashSet().insert(e) return true if it's a
+    //             // new element and false if it has been seen before
+    //             report.iter().all(|&e| uniq.insert(e))
+    //         }
+    //     })
+    //     .collect();
+    // ---------------
+    // intersting to note that we can use "{:?}" to print most data structures
+    // ---------------
+    // println!("{:?}", intermediary_result);
+
+    reports
+        .iter()
+        .filter(|report| is_safe(report))
+        .count()
+        .try_into()
+        .unwrap()
+}
+
+fn is_safe(report: &Vec<u32>) -> bool {
+    (report.is_sorted() || report.iter().rev().is_sorted())
+        && {
+            // inforces differs by at least 1
+            let mut uniq = HashSet::new();
+            // HashSet().insert(e) return true if it's a
+            // new element and false if it has been seen before
+            report.iter().all(|&e| uniq.insert(e))
+        }
+        && {
+            // https://stackoverflow.com/questions/73832075/how-to-compare-the-value-of-next-element-with-the-current-element-in-an-iterator
+            // for the .windows
+            report
+                .windows(2)
+                .all(|element_pair| element_pair[0].abs_diff(element_pair[1]) < 4)
+        }
+}
+
+fn count_safe_reports_dampened(reports: &Vec<Vec<u32>>) -> u32 {
+    reports
+        .iter()
+        .filter(|report| {
+            is_safe(report) || {
+                // check with dampening
+                (0..(report.len() - 1)).any(|index: usize| {
+                    let mut new_report = report.to_vec();
+                    new_report.remove(index.try_into().unwrap());
+                    is_safe(&new_report)
+                })
+            }
+        })
+        .count()
+        .try_into()
+        .unwrap()
+}
+
+fn main() {
+    let input = fs::read_to_string("inputs/2.txt").unwrap();
+    println!("part 1 : {}", count_safe_reports(&preprocess(&input)));
+    println!("part 2 : {}", count_safe_reports_dampened(&preprocess(&input))); // not the correct output
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = fs::read_to_string("inputs/2_e.txt").unwrap();
+        assert_eq!(count_safe_reports(&preprocess(&input)), 2);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = fs::read_to_string("inputs/2_e.txt").unwrap();
+        assert_eq!(count_safe_reports_dampened(&preprocess(&input)), 4);
+    }
+}
