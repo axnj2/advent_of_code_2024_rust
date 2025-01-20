f715e36db7b8ca54b57b68a564de3b97eae8585f
Antoine Jacques-Jourion <52408308+axnj2@users.noreply.github.com>
2025-01-20 15:50:21 +0100

    upgraded including removing copies

---
+++ src/bin/1_upgraded.rs
@@ -0,0 +1,72 @@
+use itertools::Itertools;
+use std::fs;
+
+// learned from https://github.com/voberle/adventofcode/blob/main/2024/day01/src/main.rs
+
+fn main() {
+    let exemple_input = fs::read_to_string("inputs/1_e.txt").expect("The file should have opened");
+    let exemple_lists = extract_number_lists(exemple_input);
+
+    println!(
+        "exemple : {}",
+        part1(&exemple_lists.0, &exemple_lists.1)
+    );
+
+    let full_input = fs::read_to_string("inputs/1.txt").unwrap();
+    let full_lists = extract_number_lists(full_input);
+
+    println!(
+        "step 1 : {}",
+        part1(&full_lists.0, &full_lists.1)
+    );
+
+    println!(
+        "step 2 exemple : {}",
+        part2(&exemple_lists.0, &exemple_lists.1)
+    );
+
+    println!("step 2 : {}", part2(&full_lists.0, &full_lists.1));
+}
+
+fn extract_number_lists(input: String) -> (Vec<i32>, Vec<i32>) {
+    let split_string: (Vec<i32>, Vec<i32>) = input
+        .lines()
+        .map(|line| {
+            line.trim()
+                .split_ascii_whitespace()
+                .map(|e| e.parse::<i32>().unwrap())
+                .into_iter()
+                .collect_tuple()
+                .unwrap()
+        })
+        .unzip();
+
+    return split_string;
+}
+
+fn part1(first_list: &Vec<i32>, second_list: &Vec<i32>) -> u32 {
+    first_list
+        .iter()
+        .sorted_unstable()
+        .zip(second_list.iter().sorted_unstable())
+        .map(|(e1, e2)| e1.abs_diff(*e2))
+        .sum()
+}
+
+fn part2(first_list: &Vec<i32>, second_list: &Vec<i32>) -> i32 {
+    // let mut final_value = 0;
+    // for value1 in first_list {
+    //     final_value += second_list
+    //         .clone()
+    //         .into_iter()
+    //         .filter(|value2| *value2 == value1)
+    //         .sum::<i32>()
+    // }
+
+    // return final_value;
+
+    first_list
+        .iter()
+        .map(|e1| second_list.iter().filter(|e2| *e2 == e1).sum::<i32>())
+        .sum()
+}
